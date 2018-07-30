3c1b1a1a2e2c9be066925f8f48847ea31f89d0fe
Carlos <40763512+clm987@users.noreply.github.com>
2018-07-30 12:30:16 -0300

    While 30/7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,18 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero
+	var promedio
 
+
+	while(respuesta!="n")
+		{
+			numero=prompt("ingrese un numero ")
+			promedio=numero/contador
+			contador=contador+1
+			acumulador=acumulador+numero
+			respuesta=prompt("desea seguir ingresando numeros?")
+		}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,41 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var maximo;
+	var minimo;
+	var numero;
 	var respuesta='si';
+	var bandera=0;
+	
 
 	while(respuesta!='no')
 	{
+			numero=prompt("ingrese el numero ");
+			numero=parseInt(numero);
+			
+		if (bandera==0)
+		{
+			maximo=numero;
+			minimo=numero;
+			bandera=1;
+		}
+		if (numero<minimo)
+		{
+			minimo=numero;
 		
-	
-	}
+		}
+		if (numero>maximo)
+		{
+			maximo=numero;
+		}
 
 
+		respuesta=prompt("desea seguir ingresando numeros?")
+
+	}	
+
+document.getElementById('maximo').value=maximo;
+document.getElementById('minimo').value=minimo;
 
 
 }//FIN DE LA FUNCIÓN
